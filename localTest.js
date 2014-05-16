62949e60503b15b926910f003bb4d06aed3f45e7
mruefenacht <ruefe@rcky.ch>
2014-05-16 14:04:46 +0200

    Hooked in rewrites and am currently simplyfying the api

--- localTest.js
+++ localTest.js
@@ -2,35 +2,21 @@
  * todo: add dependencies to ee-soa-request and ee-rest-headers
  * @type {exports}
  */
-var WebService      = require('ee-webservice'),
-    HTTPTransport   = require('./lib/HTTPTransport');
-
-function showPropertiesOf(instance){
-    for(var property in instance){
-        var buffer = property;
-        if(typeof instance[property] === 'function'){
-            buffer += '()';
-        }
-        console.log(buffer);
-    }
-}
-
-var service     = new WebService({port:8080, interface: 5 }),
-    transport   = new HTTPTransport(service);
+var HTTPTransport   = require('./lib/HTTPTransport');
+var websites        = [];
+var transport       = new HTTPTransport({port:8080, interface: 5 }, websites);
 
 transport.on('loading_error', function(err){
     console.log(err);
 });
 
+transport.on('request', function(request, response) {
+    // hook in your service here!
+    response.send(response.status.OK, {message: 'Yaaay'});
+});
+
 transport.onLoad(function() {
-    transport.on('request', function(request, response) {
-        console.log(request);
-        response.send(response.status.TARGET_NOT_FOUND, {});
-    });
-    console.log("halo");
-    service.listen(function(){
-        console.log("listening");
-    });
+    transport.listen();
 });
 
 transport.useTransport();
